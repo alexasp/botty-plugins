7c77d3bc14ad65b00fdcdbfa40fd15e6ec39350a
Jens Kristoffer Reitan Markussen <jkrmc12@gmail.com>
2017-02-18 18:04:24 +0100

    added nearby functions to whereto

--- whereto/__init__.py
+++ whereto/__init__.py
@@ -13,8 +13,19 @@ base_url_details    = "https://maps.googleapis.com/maps/api/place/details/json"
 oslo_latlng = "59.913869,10.752245"
 radius      = "4000" #4km
 
+user_location_map = {}
+
 def _initialise(bot):
+    plugins.register_handler(update_location, type="message", priority=5)
     plugins.register_user_command(["whereto"])
+
+def update_location(bot, event, command):
+    if event.user.is_self:
+        return
+
+    if "maps.google.com" in event.text:
+        user_location_map[event.user.id_.chat_id] = event.text.split("?q=")[1]
+        yield from bot.coro_send_message(event.conv_id, "I now know where " + event.user.full_name + " is ( ͡° ͜ʖ ͡°)")
 
 def get_map(bot, latlng):
     url = "https://maps.googleapis.com/maps/api/staticmap"
@@ -30,9 +41,18 @@ def get_map(bot, latlng):
     return io.BytesIO(raw), filename
 
 def query(bot, event, qtype, args):
+    location = oslo_latlng
+    rad = radius
+    if args[0].lower().strip() == "nearby":
+        if event.user.id_.chat_id not in user_location_map:
+            yield from bot.coro_send_message(event.conv_id, "I don't know where " + event.user.full_name + " is, please share location.")
+            return
+        rad = "600"
+        location = user_location_map[event.user.id_.chat_id]
+
     url = base_url_radar
-    url += '?location=' + oslo_latlng
-    url += '&radius=' + radius
+    url += '?location=' + location
+    url += '&radius=' + rad
     url += '&type=' + qtype
     url += '&key=' + api_key
 
