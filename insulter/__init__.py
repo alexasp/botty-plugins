6f58d843aeb4bb8178541e694ffa565e52c4d2fc
Jens Kristoffer Reitan Markussen <jkrmc12@gmail.com>
2017-02-20 22:39:33 +0100

    auto insulter

--- insulter/__init__.py
+++ insulter/__init__.py
@@ -6,9 +6,48 @@ import random
 import io
 
 logger = logging.getLogger(__name__)
+my_bros = set()
 
 def _initialise(bot):
+    plugins.register_handler(on_message, type="message", priority=5)
+    plugins.register_user_command(["bemybro"])
     plugins.register_user_command(["insult"])
+
+def on_message(bot, event, command):
+    for bro in my_bros:
+        if bro.lower() in event.text.lower():
+            yield from back_up(bro, bot, event, command)
+
+def back_up(bro, bot, event, command):
+    headers={
+      "X-Mashape-Key": "CZrByDIfctmshUZlKmwtVsg64yNSp14UJ6jjsnCSwRfXEPI0If",
+      "Content-Type": "application/x-www-form-urlencoded",
+      "Accept": "application/json"
+    }
+    url = 'https://community-sentiment.p.mashape.com/text/'
+    r = yield from aiohttp.request("post", url, headers=headers, data={
+        "txt": event.text
+    })
+    r_json = yield from r.json()
+    if r_json["result"]["sentiment"] == "Negative":
+        yield from bot.coro_send_message(event.conv_id, "I am " + r_json["result"]["confidence"] + "% sure you just insulted my bro " + bro)
+        yield from insult(bot, event, event.user.full_name.split(" ")[0])
+
+def bemybro(bot, event, *args):
+    if event.user.is_self:
+        return
+
+    if len(args) == 0:
+        yield from bot.coro_send_message(event.conv_id, "Missing argument")
+        return
+
+    name = event.user.full_name.split(" ")[0]
+    if args[0] == "start":
+        yield from bot.coro_send_message(event.conv_id, name + " is now my bro")
+        my_bros.add(name)
+    elif args[0] == "stop":
+        yield from bot.coro_send_message(event.conv_id, name + " is no longer my bro, but I still love him")
+        my_bros.remove(name)
 
 def insult(bot, event, *args):
     if event.user.is_self:
