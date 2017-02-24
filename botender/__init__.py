75111b8c34f5c582aab700a94efa73bab07599aa
Jens Kristoffer Reitan Markussen <jkrmc12@gmail.com>
2017-02-24 23:24:52 +0100

    danger zone and fixed botender

--- botender/__init__.py
+++ botender/__init__.py
@@ -30,31 +30,31 @@ def get_image(drink_id):
 
 def get_one_here(bot, location):
 	logger.info(location)
-	r = yield from aiohttp.request("get", url + 'illhaveones/near/' + location + ',50/?apiKey=' + api_key)				
+	r = yield from aiohttp.request("get", url + 'illhaveones/near/' + location + ',50/?apiKey=' + api_key)
 	r_json = yield from r.json()
 	if len(r_json["result"]) == 0:
 		return False, '', None
-	
+
 	drink = r_json["result"][random.randint(0, len(r_json["result"])-1)]
 	image_data, filename = yield from get_image(drink["drink"]["id"])
 	image_id = yield from bot._client.upload_image(image_data, filename=filename)
 	return True, drink["drink"]["text"], image_id
-	
+
 def illhaveone(bot, event, *args):
 	if event.user.is_self:
 		return
-	
+
 	if len(args) == 0 or args[0].lower() == 'in':
 		where = 'oslo'
 		if len(args) > 1:
 			where = args[1]
-		
-		r = yield from aiohttp.request("get", url + 'illhaveones/incity/' + where + '/?apiKey=' + api_key)				
+
+		r = yield from aiohttp.request("get", url + 'illhaveones/incity/' + where + '/?apiKey=' + api_key)
 		r_json = yield from r.json()
 		if len(r_json["result"]) == 0:
 			yield from bot.coro_send_message(event.conv_id, "sry, I duno what u can have in " + where)
 			return
-		
+
 		drink = r_json["result"][random.randint(0, len(r_json["result"])-1)]
 		image_data, filename = yield from get_image(drink["drink"]["id"])
 		image_id = yield from bot._client.upload_image(image_data, filename=filename)
@@ -62,14 +62,14 @@ def illhaveone(bot, event, *args):
 		try:
 			success, name, loc_url, image_id = yield from bot.call_shared("where.placeat", bot, str(drink["latitude"]) + "," + str(drink["longitude"]), 'bar')
 			if success:
-				yield from bot.coro_send_message(event.conv_id, "At " + name + "(" + loc_url + ")", image_id=image_id)
+				yield from bot.coro_send_message(event.conv_id, "At " + name + ". find it: " + loc_url + "", image_id=image_id)
 		except Exception as e:
 			yield from bot.coro_send_message(event.conv_id, "I depend on 'whereto' to werk, pliz tell Alex to load it")
 			logger.info (e)
 	elif args[0].lower() == 'here':
 		try:
 			success, location = bot.call_shared("where.ami", bot, event.user)
-			if success:				
+			if success:
 				success, name, image_id = yield from get_one_here(bot, location)
 				if success:
 					yield from bot.coro_send_message(event.conv_id, "You can have a <b>" + name + "</b>", image_id=image_id)
@@ -80,50 +80,60 @@ def illhaveone(bot, event, *args):
 		except:
 			yield from bot.coro_send_message(event.conv_id, "I depend on 'whereto' to werk, pliz tell Alex to load it")
 
-	
+
 def mixme(bot, event, *args):
-	if event.user.is_self:
-		return
+    if event.user.is_self:
+        return
 
-	ingredient = None
-	taste = None
-	term = []
-	if len(args) == 0:
-		ingredient = known_types[random.randint(0,len(known_types)-1)]
-	else:
-		for arg in args:
-			if arg.lower() in known_types and ingredient is None:
-				ingredient = arg.lower()
-			elif arg.lower() in known_tastes and taste is None:
-				taste = arg.lower()
-			else:
-				term.append(arg)
+    ingredient = None
+    taste = None
+    term = []
+    if len(args) == 0:
+        ingredient = known_types[random.randint(0,len(known_types)-1)]
+    else:
+        for arg in args:
+            if arg.lower() in known_types and ingredient is None:
+                ingredient = arg.lower()
+            elif arg.lower() in known_tastes and taste is None:
+                taste = arg.lower()
+            else:
+                term.append(arg)
 
-	url_append = ''
-	if ingredient is not None or taste is not None:
-		url_append = 'drinks/alcoholic/rating/gt50/'
-		if ingredient is not None:
-			url_append += 'withtype/' + ingredient + '/'
-		if taste is not None:
-			url_append += 'tasting/' + taste + '/'
-	else:
-		url_append = 'quickSearch/drinks/' + "+".join(term)
-		
-	r = yield from aiohttp.request("get", url + url_append + '/?apiKey=' + api_key)	
-	r_json = yield from r.json()
-	if r_json["totalResult"] == 0:
-		yield from bot.coro_send_message(event.conv_id, "I duno how to mix with " + ingredient)
-		return
+    url_append = ''
+    if len(term) > 0:
+        if ingredient is not None:
+            term.append(ingredient)
+        if taste is not None:
+            term.append(taste)
+        url_append = 'quickSearch/drinks/' + "%20".join(term)
+    elif ingredient is not None or taste is not None:
+        url_append = 'drinks/alcoholic/rating/gt50/'
+        if ingredient is not None:
+            url_append += 'withtype/' + ingredient + '/'
+        if taste is not None:
+            url_append += 'tasting/' + taste + '/'
+    else:
+        yield from bot.coro_send_message(event.conv_id, "I duno how to mix that")
+        return
 
-	drink = r_json["result"][random.randint(0, len(r_json["result"])-1)]
 
-	result = "<b>" + drink["name"] + "</b>\n"
-	result += drink["descriptionPlain"] +  " <i>Served in a " + drink["servedIn"]["text"].lower() + "</i> \n"
-	ingedirents = "\n<b>Ingredients:</b>\n"
-	for ing in drink["ingredients"]:
-		ingedirents += '- ' + ing["textPlain"] + "\n"
-	result += ingedirents
+    r_url = url + url_append + '/?apiKey=' + api_key
+    logger.info(r_url)
+    r = yield from aiohttp.request("get", r_url)
+    r_json = yield from r.json()
+    if r_json["totalResult"] == 0:
+        yield from bot.coro_send_message(event.conv_id, "I duno how to mix that")
+        return
 
-	image_data, filename = yield from get_image(drink["id"])
-	image_id = yield from bot._client.upload_image(image_data, filename=filename)
-	yield from bot.coro_send_message(event.conv_id, result, image_id=image_id)
+    drink = r_json["result"][random.randint(0, len(r_json["result"])-1)]
+
+    result = "<b>" + drink["name"] + "</b>\n"
+    result += drink["descriptionPlain"] +  " <i>Served in a " + drink["servedIn"]["text"].lower() + "</i> \n"
+    ingedirents = "\n<b>Ingredients:</b>\n"
+    for ing in drink["ingredients"]:
+        ingedirents += '- ' + ing["textPlain"] + "\n"
+    result += ingedirents
+
+    image_data, filename = yield from get_image(drink["id"])
+    image_id = yield from bot._client.upload_image(image_data, filename=filename)
+    yield from bot.coro_send_message(event.conv_id, result, image_id=image_id)
---
+++ dangerzone/__init__.py
@@ -0,0 +1,98 @@
+import asyncio, aiohttp, re, os
+import plugins
+import logging
+import random
+import io
+
+logger = logging.getLogger(__name__)
+client_id = 'b144351ffb05838'
+url = 'https://api.imgur.com/3/gallery/r/FiftyFifty/time/'
+img_url = 'https://api.imgur.com/3/gallery/r/FiftyFifty/'
+headers = {'Authorization': 'Client-ID ' + client_id, 'Accept': 'application/json'}
+balls_being_checked = {}
+
+approved_convs = set()
+
+def _initialise(bot):
+    plugins.register_handler(on_message, type="message", priority=5)
+    plugins.register_user_command(["checkmyballs"])
+    plugins.register_admin_command(["takeustothedangerzone", "getusoutofthedangerzone"])
+
+def takeustothedangerzone(bot, event, *args):
+    if event.user.is_self:
+        return
+    yield from bot.coro_send_message(event.conv_id, "All abord the highway to the danger zone")
+    approved_convs.add(event.conv_id)
+
+def getusoutofthedangerzone(bot, event, *args):
+    if event.user.is_self:
+        return
+    yield from bot.coro_send_message(event.conv_id, "Getting the FUCK out of the danger zone")
+    approved_convs.remove(event.conv_id)
+
+def on_message(bot, event, command):
+    if event.user.is_self:
+        return
+
+    global balls_being_checked
+    user_id = event.user.id_.chat_id
+
+    if user_id not in balls_being_checked:
+        return
+    if balls_being_checked[user_id][0] is False:
+        return
+
+    if "i lack the balls" == event.text.lower().strip():
+        balls_being_checked[user_id] = (False, None)
+        yield from bot.coro_send_message(event.conv_id, event.user.full_name + " lacks the balls")
+    elif "i have the balls" == event.text.lower().strip():
+        link = balls_being_checked[user_id][1]
+        logger.info(link)
+
+        if event.conv_id not in approved_convs:
+            yield from bot.coro_send_message(event.conv_id, "Alex doesn't want to take us on to the highway to the danger zone")
+            return
+
+        filename = os.path.basename(link)
+        r = yield from aiohttp.request('get', link)
+        raw = yield from r.read()
+        image_data = io.BytesIO(raw)
+        image_id = yield from bot._client.upload_image(image_data, filename=filename)
+        yield from bot.coro_send_message(event.conv.id_, None, image_id=image_id)
+        balls_being_checked[user_id] = (False, None)
+
+def checkmyballs(bot, event, *args):
+    if event.user.is_self:
+        return
+
+    if event.conv_id not in approved_convs:
+        yield from bot.coro_send_message(event.conv_id, "Alex doesn't want to take us on to the highway to the danger zone")
+        return
+
+    global balls_being_checked
+    user_id = event.user.id_.chat_id
+    if user_id in balls_being_checked and balls_being_checked[user_id][0] is True:
+        yield from bot.coro_send_message(event.conv_id, event.user.full_name + " is already getting his balls checked")
+        return
+
+    r = yield from aiohttp.request('get', url + str(random.randint(0, 10)) +'/year', headers=headers)
+    r_json = yield from r.json()
+    if len(r_json["data"]) == 0:
+        yield from bot.coro_send_message(event.conv_id, "Failed to find image :(")
+        return
+
+    sel = r_json["data"][random.randint(0, len(r_json["data"])-1)]
+    title = re.sub(r"\[.+?\]|\(.+?\)|NSFW(\/L)?|SFW", "", sel["title"])
+    titles = title.split("|")
+    if len(titles) != 2:
+        yield from bot.coro_send_message(event.conv_id, "I failed to parse reddit title :(")
+        return
+
+    r = yield from aiohttp.request('get', img_url + sel["id"], headers=headers)
+    r_json = yield from r.json()
+    link = r_json["data"]["link"]
+
+    yield from bot.coro_send_message(event.conv_id, "<b>" + titles[0].strip() + "</b> or <b>" + titles[1].strip() + "</b>")
+
+    balls_being_checked[user_id] = (True, link)
+    yield from bot.coro_send_message(event.conv_id, "So, " + event.user.full_name +  " do you have the balls, or do you lack the balls??")
