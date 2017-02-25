13e4094e1c6243cfc238168c54d28d429b4e5595
Jens Kristoffer Reitan Markussen <jkrmc12@gmail.com>
2017-02-25 18:21:27 +0100

    so meta

--- metaplug/__init__.py
+++ metaplug/__init__.py
@@ -6,8 +6,71 @@ from asyncio.subprocess import PIPE
 logger = logging.getLogger(__name__)
 
 def _initialise(bot):
+    plugins.register_handler(on_botting, type="sending", priority=1)
     plugins.register_admin_command(["pulltheplugs"])
-    plugins.register_user_command(["tuklecheck"])
+    plugins.register_user_command(["tuklecheck", "amioutdated", "versionof"])
+
+def versionof(bot, event, *args):
+    if event.user.is_self:
+        return
+
+    if len(args) == 0:
+        yield from bot.coro_send_message(event.conv_id, "cannot check the version of nothing")
+        return
+
+    plug = args[0].strip()
+    try:
+        guid = yield from bot.call_shared(plug + ".version", bot)
+        yield from bot.coro_send_message(event.conv_id, "<b>Plugin: </b><i>" + plug  + "</i> was loaded with commit guid " + guid)
+        current_guid = yield from get_last_commit()
+        if guid != current_guid:
+            yield from bot.coro_send_message(event.conv_id, "Plugin is not up to date!\n <b>current commit guid:</b> " + current_guid)
+    except Exception as e:
+        logger.info(e)
+        yield from bot.coro_send_message(event.conv_id, "I've not registerd that " + plug + " was loaded")
+
+def get_last_commit():
+    proc = yield from asyncio.create_subprocess_exec(*["git", "rev-parse", "--verify", "HEAD"],
+        stdout=PIPE, stderr=PIPE, env=dict(os.environ), cwd="hangupsbot/plugins/botty-plugins/")
+    stdout, err = yield from proc.communicate()
+    logger.info(err.decode())
+    return stdout.decode()
+
+def on_botting(bot, broadcast_list, context):
+    segments = broadcast_list[0][1]
+
+    if "botty-plugins" in segments[0].text:
+        if "loaded" in segments[1].text or "reloaded" in segments[1].text:
+            botty_plug = re.findall(r"(?<=botty\-plugins\.)\w*", segments[0].text)[0]
+            current_guid = get_last_commit()
+            bot.register_shared(botty_plug + ".version", lambda x: current_guid)
+
+    if "reloading config.json" == segments[0].text:
+        plugs = bot.config["plugins"]
+        for plug in plugs:
+            logger.info(plug)
+            tmp = re.findall(r"(?<=botty\-plugins\.)\w*", plug)
+            if len(tmp) > 0:
+                botty_plug = tmp[0]
+                current_guid = get_last_commit()
+                bot.register_shared(botty_plug + ".version", lambda x: current_guid)
+
+
+def amioutdated(bot, event, *args):
+    if event.user.is_self:
+        return
+
+    proc = yield from asyncio.create_subprocess_exec(*["git", "remote", "update"],
+        stdout=PIPE, stderr=PIPE, env=dict(os.environ), cwd="hangupsbot/plugins/botty-plugins/")
+    stdout, err = yield from proc.communicate()
+    yield from bot.coro_send_message(event.conv_id, err.decode())
+
+    proc = yield from asyncio.create_subprocess_exec(*["git", "show-branch", "*master"],
+        stdout=PIPE, stderr=PIPE, env=dict(os.environ), cwd="hangupsbot/plugins/botty-plugins/")
+    stdout, err = yield from proc.communicate()
+    status = stdout.decode()
+    yield from bot.coro_send_message(event.conv_id, err.decode())
+    yield from bot.coro_send_message(event.conv_id, stdout.decode())
 
 def pulltheplugs(bot, event, *args):
     if event.user.is_self:
@@ -20,7 +83,7 @@ def pulltheplugs(bot, event, *args):
         status = stdout.decode()
         yield from bot.coro_send_message(event.conv_id, err.decode())
         yield from bot.coro_send_message(event.conv_id, stdout.decode())
-        
+
 
     proc = yield from asyncio.create_subprocess_exec(*["git", "pull", "--no-commit", "--no-edit"],
         stdout=PIPE, stderr=PIPE, env=dict(os.environ), cwd="hangupsbot/plugins/botty-plugins/")
