e5ddf134d1ac0154576335ca4ec5f2cb27016d79
Jens Kristoffer Reitan Markussen <jkrmc12@gmail.com>
2017-02-27 22:57:07 +0100

    forgot a plug

--- __init__.py
+++ __init__.py
@@ -1,9 +1,11 @@
-import plugins
+import plugins, logging
+logger = logging.getLogger(__name__)
 
 plugs = [
     "metaplug",
     "askdonald",
     "askwolfy",
+    "botender",
     "dangerzone",
     "imgur",
     "insulter",
@@ -18,7 +20,11 @@ plugs = [
     "whereto"
 ]
 
+
 def _initialise(bot):
     for plug in plugs:
-        plugins.unload(bot, "plugins.botty-plugins." + plug)
-        plugins.load(bot, "plugins.botty-plugins." + plug)
+        try:
+            plugins.unload(bot, "plugins.botty-plugins." + plug)
+            plugins.load(bot, "plugins.botty-plugins." + plug)
+        except (RuntimeError, KeyError) as e:
+            logger.info(str(e))
